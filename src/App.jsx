0e08acc6b676782019454ea6440141652815fc21
David Peter <amadip272@gmail.com>
2025-11-02 22:10:10 +0100

    Fix import statement formatting in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
-import {useState} from 'react'
+import {useState} from 'react';
 
 const App = () => {
 
